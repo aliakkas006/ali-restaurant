0eb5ffefc58ac5e749c1c10d9d73065c95d85747
Ali Akkas <aliakkas006@gmail.com>
2022-02-16 23:36:51 +0600

    chef and menu section added

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
+import { meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef();
+
+  return (
+    <div className='app__video'>
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className='app__video-overlay flex__center'>
+        <div className='app__video-overlay_circle flex__center'>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Intro;
