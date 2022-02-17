5db92679df1d96b647b98c77198a4aed90386901
Ali Akkas <aliakkas006@gmail.com>
2022-02-17 19:40:28 +0600

    ui completed

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
 import { meal } from '../../constants';
 import './Intro.css';
@@ -6,6 +7,15 @@ import './Intro.css';
 const Intro = () => {
   const [playVideo, setPlayVideo] = useState(false);
   const vidRef = useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playVideo)
+      vidRef.current.pause();
+    else
+      vidRef.current.play();
+  }
 
   return (
     <div className='app__video'>
@@ -18,8 +28,14 @@ const Intro = () => {
         muted
       />
       <div className='app__video-overlay flex__center'>
-        <div className='app__video-overlay_circle flex__center'>
-          
+        <div className='app__video-overlay_circle flex__center'
+          onClick={handleVideo}
+        >
+          {
+            playVideo
+              ? <BsPauseFill color='#fff' fontSize={30} />
+              : <BsFillPlayFill color='#fff' fontSize={30} />
+          }
         </div>
       </div>
     </div>
